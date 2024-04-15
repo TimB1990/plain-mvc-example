b1437eeac0819b97ab436da5a4f03b0a441500dc
tboon <t.boons@live.nl>
2024-04-15 22:42:44 +0200

    Add toggle buttons for product view templates

--- app/views/products/index.php
+++ app/views/products/index.php
@@ -3,6 +3,15 @@
         <ul>
             <li style="color:grey;"><a href="#">Add Category</a></li>
             <li><a href="products/create">Add Single Product</a></li>
+            <li style="float:right;">
+                <a href="products" class="button-link">
+                    <img src="../app/static/icons/format-list-group-default.svg" alt="default-product-list-view">
+                </a>
+                <a href="products?categories=1" class="button-link">
+                    <img src="../app/static/icons/format-list-group-plus.svg
+                " alt="hierarchy-product-list-view">
+                </a>
+            </li>
         </ul>
     </nav>
 
